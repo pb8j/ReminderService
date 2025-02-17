4af1254a591dfa47c469e1aebbd01032ad8962fa
Puru Brahmankar <purubrahmankar8@gmail.com>
2025-02-17 16:04:01 +0530

    Setup opf smtp using nodemailer done and Added notification ticket model

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cron = require('node-cron')
 
 const { PORT } = require('./config/serverConfig');
+
+const {sendBasicEmail} = require('./services/email-service');
 
 const setupAndStartServer = () => {
     const app = express();
@@ -10,6 +13,17 @@ const setupAndStartServer = () => {
 
     app.listen(PORT, () => {
         console.log(`Server started at port ${PORT}`);
+
+        // sendBasicEmail(
+        //     'support@admin.com',
+        //     'purubackend0@gmail.com',
+        //     'This is a dummy email',
+        //     'Hey, how are you, I hope you like the support'
+        // );
+
+        cron.schedule('*/2 * * * *', () => {
+            console.log('running a task every two minutes');
+        });
     });
 }
 
