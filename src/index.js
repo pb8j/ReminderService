1474792eca0406cea5046b3e5810e6bf69ef704f
Puru Brahmankar <purubrahmankar8@gmail.com>
2025-02-19 13:42:57 +0530

    updated notification cron

--- src/index.js
+++ src/index.js
@@ -1,30 +1,38 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+'use strict';
 
-const { PORT } = require('./config/serverConfig');
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const process = require('process');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
+const db = {};
 
-// const { sendBasicEmail } = require('./services/email-service');
-
-const cron = require('node-cron');
-
-const setupAndStartServer = () => {
-    const app = express();
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended: true}));
-
-    app.listen(PORT, () => {
-        console.log(`Server started at port ${PORT}`);
-
-        // sendBasicEmail(
-        //     'support@admin.com',
-        //     'moviebookingappservice@gmail.com',
-        //     'This is a testing email',
-        //     'Hey, how are you, I hope you like the support'
-        // );
-        cron.schedule('*/2 * * * *', () => {
-            console.log('running a task every two minutes');
-          });          
-    });
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-setupAndStartServer();
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+  })
+  .forEach(file => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
