3ea042a211bf0c0da9f0c0ee9a20360478a46158
Puru Brahmankar <purubrahmankar8@gmail.com>
2025-02-22 12:39:38 +0530

    Added create api Ticket

--- src/index.js
+++ src/index.js
@@ -1,38 +1,30 @@
-'use strict';
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const process = require('process');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
-const db = {};
+const { PORT } = require('./config/serverConfig');
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+const TicketController = require('./controllers/ticket-controller');
+const EmailService = require('./services/email-service');
+
+const jobs = require('./utils/job');
+const { subscribeMessage, createChannel } = require('./utils/messageQueue');
+const { REMINDER_BINDING_KEY } = require('./config/serverConfig');
+
+const setupAndStartServer = async () => {
+    const app = express();
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+
+    app.post('/api/v1/tickets', TicketController.create);
+
+    const channel = await createChannel();
+    subscribeMessage(channel, EmailService.subscribeEvents, REMINDER_BINDING_KEY);
+
+    app.listen(PORT, () => {
+        console.log(`Server started at port ${PORT}`);
+        // jobs();
+        
+    });
 }
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
-
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
-module.exports = db;
+setupAndStartServer();
